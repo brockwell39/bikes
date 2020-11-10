2bf33a74686d576e1e6727d167e34eacc348f040
Alex Catlow <alexcatlow@Alexs-MacBook-Pro.local>
2020-11-10 11:26:06 +0000

    With migrations

--- src/Application.php
+++ src/Application.php
@@ -34,6 +34,8 @@ class Application extends BaseApplication
      */
     public function bootstrap()
     {
+        $this->addPlugin('Migrations');
+
         $this->addPlugin('Josegonzalez/Upload');
 
         $this->addPlugin('Josegonzalez/Upload');
